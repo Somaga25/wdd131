ed85769c9f0c0feb461fa2dd2f26154b0917e418
Somaga25 <ceke2@byupathway.edu>
2026-03-29 00:09:25 +0100

    improvement on the place html

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,11 +1,8 @@
-// FOOTER DATES
-const yearElement = document.getElementById("year");
-const modifiedElement = document.getElementById("modified");
+// FOOTER
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("modified").textContent = document.lastModified;
 
-yearElement.textContent = new Date().getFullYear();
-modifiedElement.textContent = document.lastModified;
-
-// STATIC WEATHER VALUES
+// WEATHER VALUES
 const temperature = 30;
 const windSpeed = 10;
 
@@ -14,12 +11,12 @@ function calculateWindChill(t, v) {
 return 13.12 + 0.6215 * t - 11.37 * v**0.16 + 0.3965 * t * v**0.16;
 }
 
-// DETERMINE WIND CHILL
-let windChillValue = "N/A";
+// APPLY CONDITIONS
+let windChill = "N/A";
 
 if (temperature <= 10 && windSpeed > 4.8) {
-windChillValue = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
 }
 
-// DISPLAY WIND CHILL
-document.getElementById("windchill").textContent = windChillValue;
+// DISPLAY
+document.getElementById("windchill").textContent = windChill;
